4348930d938f7ff5a42f3f847c58ffc0c80f222b
moxDroid Labs Inc <pritam.world@gmail.com>
2019-02-14 18:26:08 -0500

    Initial Commit

---
+++ SwiftInheritanceExample/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftInheritanceExample
+//
+//  Created by moxDroid on 2019-02-14.
+//  Copyright © 2019 moxDroid. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
