9bff903b468e45c340aef8757f1a97cee1b93ebb
moxDroid Labs Inc <pritam.world@gmail.com>
2019-02-14 19:00:22 -0500

    Faculty Class added

---
+++ SwiftInheritanceExample/Faculty.swift
@@ -0,0 +1,14 @@
+//
+//  Faculty.swift
+//  SwiftInheritanceExample
+//
+//  Created by moxDroid on 2019-02-14.
+//  Copyright © 2019 moxDroid. All rights reserved.
+//
+
+import Foundation
+
+class Faculty
+{
+    
+}
